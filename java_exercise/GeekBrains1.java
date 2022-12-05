c556ffa990e72247b6de8944aee1c4a2b76ee7e7
Shuleole <thisisworkingzone@gmail.com>
2022-12-05 18:40:20 +0300

    all shit

---
+++ java_exercise/GeekBrains1.java
@@ -0,0 +1,10 @@
+
+class GeekBrains1 {
+	public static void main(String[] args) {
+		int k = 2;
+		int b = 5;
+		int result = (int) ((Math.random()*100)*(Math.pow(b, k)) + (Math.random()*100));
+		System.out.println(result);
+		
+	}
+}

7d9d99eb0d5a9b50b0422a26e5c5aec27249c4cf
Shuleole <thisisworkingzone@gmail.com>
2022-12-19 09:17:09 +0300

    add Homework class

---
+++ java_exercise/Homework.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Homework {
+	
+	public static void main (String[] args) {
+	
+		List<Integer> customList = new ArrayList<>();
+		for (int i = 1; i < 30; i++) {
+			customList.add((int)(Math.random() * i));
+		}
+	/*	for (int i = 0; i < customList.size() - 1; i++) {
+			System.out.println(customList.get(i));
+		} */
+		
+		Homework HW = new Homework();
+		
+		List<Integer> oneList = HW.removeNegativeValue(customList);
+		for (int i = 0; i < oneList.size() - 1; i++) {
+			System.out.println(oneList.get(i));
+		}
+		
+	}
+	
+	public List<Integer> removeNegativeValue(List<Integer> List) {
+		
+		ArrayList<Integer> removedNegativeValueList = new ArrayList<>();
+		for (int i = 0; i < List.size() - 1; i++) {
+			if ((List.get(i) % 2) == 0) {
+				removedNegativeValueList.add(List.get(i));
+			}
+		}
+		return removedNegativeValueList;
+	}
+}
